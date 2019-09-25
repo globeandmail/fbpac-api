075790c90f2feecbdc48efd5c8645f8254730e19
Jeremy B. Merrill <jmerrill@qz.com>
2019-09-25 11:38:44 +0200

    adds additional API endpoints for paid for by, first_seen/not

--- config/routes.rb
+++ config/routes.rb
@@ -24,22 +24,44 @@ Rails.application.routes.draw do
       end
 
       collection do 
-        get "persona"
         get "top_advertising_methods"
         get "summarize"
-        get "homepage_stats"
-        get "write_homepage_stats"
+        get "advertiser"
+        get "by_state"
+
+        # for ProPublica 2018 homepage
+        # get "persona"
+        # get "homepage_stats"
+        # get "write_homepage_stats"
+
+
         get "by_segments"
         get "by_advertisers"
         get "by_targets"
+        get "by_paid_for_by"
+
         get "this_month_segments"
         get "this_month_advertisers"
         get "this_month_targets"
+        get "this_month_paid_for_bys"
+
         get "this_week_segments"
         get "this_week_advertisers"
         get "this_week_targets"
-        get "advertiser"
-        get "by_state"
+        get "this_week_paid_for_bys"
+
+
+        get "this_week_advertisers_first_seen"
+        get "this_week_targets_first_seen" # usually blank, maybe not worth displaying
+        get "this_week_segments_first_seen"
+        get "this_week_paid_for_by_first_seen"
+
+        get "this_month_advertisers_first_seen"
+        get "this_month_targets_first_seen"
+        get "this_month_segments_first_seen"
+        get "this_month_paid_for_bys_first_seen"
+
+        # get "pivot/:kind/:timeperiod/:first_or_not"
       end
     end
   end
